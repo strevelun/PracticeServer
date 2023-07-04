defcf56dae80ff1553e77a55f478651f4e7bb405
Eunho Bae <fere1032@gmail.com>
2023-07-05 01:55:17 +0900

    과제3-1

--- TestClient/TestClient/TestClient.cpp
+++ TestClient/TestClient/TestClient.cpp
@@ -10,61 +10,95 @@
 
 using namespace std;
 
-// 나중에 들어온 클라는 지금까지 나눴던 데이터 10줄 정보 가져옴.
-
 #pragma comment( lib, "ws2_32.lib")
 
-list<const char*> conversationList;
+enum class ePacketType
+{
+	PlayerEnter,
+	PlayerExit,
+	PlayerConnect,
+	PlayerDisconnect,
+	TimeOut,
+	GameStart,
+	GameEnd
+};
+
+typedef struct _packetData
+{
+	int size;
+	char* body;
+} tPacketData;
+
+const char* printChoice[4] = { "선택안함", "가위", "바위", "보" };
+queue<const char*> recvList;
+vector<SOCKET> listSocket;
 bool isEntered = false;
-int i = 0;
-char input[100] = "";
+char input;
 SOCKET   hSocket;
+HANDLE hThread;
 bool isUsed = false;
 bool isEnd = false;
-//queue<float> queueMsgElapsed;
 
-void PrintBoard()
-{
-	if (!isUsed)
-	{ 
-		isUsed = true;
-		system("cls");
-		for (int i = 0; i < 10 - conversationList.size(); i++) puts("");
-		list<const char*>::iterator iter = conversationList.begin();
-		list<const char*>::iterator iterEnd = conversationList.end();
-		for (; iter != iterEnd; iter++)
-			printf("%s\n", *iter);
-		puts("=========================================================");
-		printf("입력 : %s\n", input);
-		isUsed = false;
-	}
-}
+int player;
+bool gameStart = false;
+bool gamePlaying = false;
 
-unsigned int __stdcall Comm(void* _pArgs)
-{
-	while (1)
-	{
-		int tempSize;
-		char temp[100];
-		tempSize = recv(hSocket, temp, sizeof(temp), 0);
+LARGE_INTEGER timer, start, end1;
+float deltaTime, elapsed;
 
-		if (tempSize != SOCKET_ERROR)
-		{
-			char* inputTemp = new char[strlen(temp) + 1];
-			strcpy_s(inputTemp, strlen(temp) + 1, temp);
-			if (strcmp("▒", inputTemp) == 0)
-			{
-				isEnd = true;
-				break;
-			}
-			conversationList.push_back(inputTemp);
-			PrintBoard();
-		}
-	}
-	return -1;
-}
+template<typename T>
+tPacketData WritePacket(ePacketType what, T& body);
+int ReadPacket(char* body, int size);
+void PrintBoard();
+unsigned int __stdcall Comm(void* _pArgs);
+void Input();
+void Update();
+int Init();
+
+#define TIMEOUT			10.0f
 
 int main(void)
+{
+	if (Init() == -1) return -1;
+
+	while (1)
+	{
+		if (gameStart)
+		{
+			QueryPerformanceCounter(&start);
+			gameStart = false;
+			gamePlaying = true;
+		}
+
+		if (gamePlaying)
+		{
+			QueryPerformanceCounter(&end1);
+			deltaTime = (end1.QuadPart - start.QuadPart) / (float)timer.QuadPart;
+			elapsed += deltaTime; // 서버에서 stop할 때까지 계속 측정
+			start = end1;
+
+			if (!isUsed)
+			{
+				SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,14 });
+				printf("                               ");
+				printf("타이머 : %.2f\n", TIMEOUT - elapsed);
+			}
+		}
+
+		Input();
+
+		if (isEnd) break;
+	}
+
+	closesocket(hSocket);
+	WSACleanup();
+	CloseHandle(hThread);
+	system("pause");
+
+	return 0;
+}
+
+int Init()
 {
 	WSADATA  wsaData;
 	SOCKADDR_IN  servAddr;
@@ -93,62 +127,201 @@ int main(void)
 		return -1;
 	}
 
+	srand(time(nullptr));
 	unsigned int threadID;
-	HANDLE hThread;
 	hThread = (HANDLE)_beginthreadex(NULL, 0, &Comm, nullptr, 0, &threadID);
 
-	PrintBoard();
 
-	srand(time(nullptr));
-
-	LARGE_INTEGER timer, start, end;
-	float deltaTime, elapsed = 0.0f;
 	QueryPerformanceFrequency(&timer);
+}
 
+void PrintBoard()
+{
+
+	if (!isUsed)
+	{
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
+		isUsed = true;
+		int size = listSocket.size();
+
+		system("cls");
+		printf("본인 : %d\n", player);
+		puts("================== 가위바위보 게임 ==================");
+		if (size == 0) puts("");
+		int i = 0;
+		for (; i <= 1; i++)
+			if (i <= (size - 1))
+				printf("플레이어%d : %d\n", i + 1, (int)listSocket[i]);
+		puts("================== 대기열 ==================");
+		for (; i < size; i++)
+			printf("%d ", (int)listSocket[i]);
+		puts("\n============================================");
+		if (gamePlaying)
+		{
+			//printf("\t\t타이머 : %f\n", TIMEOUT - elapsed);
+			int ip = input == 0 ? 0 : input - '0';
+			printf("입력 : %s", printChoice[ip]);
+		}
+		isUsed = false;
+	}
+}
+
+unsigned int __stdcall Comm(void* _pArgs)
+{
 	while (1)
 	{
+		int tempSize;
+		char temp[100] = { 0 };
+		tempSize = recv(hSocket, temp, sizeof(temp), 0);
 
-		if (_kbhit())
+		if (tempSize != SOCKET_ERROR)
 		{
-			char ch = _getch();
-			if (ch == '\r')
-			{
-				if (strlen(input) > 0)
-				{
-					send(hSocket, input, strlen(input) + 1, 0);
-					memset(input, 0, sizeof(input));
-					//queueMsgElapsed.push(elapsed);
-					i = 0;
-				}
-			}
-			else if (ch == '\b')
-			{
-				if (i >= 1)
-					input[--i] = 0;
-			}
-			else if (ch == 27) // esc키
-			{
-				send(hSocket, "▒", strlen("▒") + 1, 0);
-			}
-			else
-				input[i++] = ch;
-			PrintBoard();
-		}
+			char* inputTemp = new char[tempSize];
+			memcpy(inputTemp, &temp, tempSize);
 
-		if (isEnd) break;
+			int type = ReadPacket(inputTemp, tempSize);
 
-		if (conversationList.size() > 10)
-		{
-			delete[] conversationList.front();
-			conversationList.pop_front();
+			//for (int i = 0; i < tempSize; i++)
+			//	printf("%02x ", inputTemp[i]);
+			//puts("");
+
+			if (type == (int)ePacketType::PlayerDisconnect)
+			{
+				isEnd = true;
+
+				break;
+			}
+
 			PrintBoard();
 		}
 	}
+	return -1;
+}
 
-	closesocket(hSocket);
-	WSACleanup();
-	CloseHandle(hThread);
+template<typename T>
+tPacketData WritePacket(ePacketType what, T& body)
+{
+	u_short size = sizeof(u_short) + sizeof(body);
+	char* packet = new char[size + sizeof(u_short)];
+	char* temp = packet;
+	memcpy(temp, &size, sizeof(u_short));
+	temp += sizeof(u_short);
+	memcpy(temp, &what, sizeof(u_short));
+	temp += sizeof(u_short);
+	memcpy(temp, &body, sizeof(body));
+	//temp += sizeof(body);
+	tPacketData data = { size + sizeof(u_short), packet };
+	return data;
+}
 
-	system("pause");
-	return 0;
+// char* packet , recvedSize
+int ReadPacket(char* body, int size)
+{
+	int count = 0;
+	char* temp = body;  // packet
+
+
+
+	while (count + sizeof(u_short) < size)
+	{
+		// size 
+		u_short header = *(u_short*)temp;
+		count += header;
+		temp += sizeof(u_short);
+
+		u_short type = *(u_short*)temp;
+		temp += sizeof(u_short);
+		if (type == (u_short)ePacketType::PlayerEnter) // 누군가 처음 들어왔을떄
+		{
+			int id = *(u_short*)temp;
+			listSocket.push_back(id);
+		}
+		else if (type == (u_short)ePacketType::PlayerConnect) // 가장 마지막에 있는게 나 자신
+		{
+			int listSize = (header - sizeof(u_short)) / sizeof(u_short);
+			int id = -1;
+			for (int i = 0; i < listSize; i++)
+			{
+				id = *(u_short*)temp;
+				listSocket.push_back(id);
+				temp += sizeof(u_short);
+			}
+			player = id;
+			continue;
+		}
+		else if (type == (u_short)ePacketType::PlayerDisconnect)
+		{
+			return type;
+		}
+		else if (type == (u_short)ePacketType::PlayerExit)
+		{
+			SOCKET disconnectedSocket = *(u_short*)temp;
+			vector<SOCKET>::iterator iter = listSocket.begin();
+			vector<SOCKET>::iterator iterEnd = listSocket.end();
+
+			for (; iter != iterEnd; )
+			{
+				if (*iter == disconnectedSocket)
+				{
+					listSocket.erase(iter);
+					break;
+				}
+			}
+			if (listSocket.size() < 2) gamePlaying = false;
+		}
+		else if (type == (u_short)ePacketType::GameStart)
+		{
+			gameStart = true;
+			input = 0;
+		}
+		else if (type == (u_short)ePacketType::GameEnd)
+		{
+			gameStart = false;
+			elapsed = 0.0f;
+			input = 0;
+		}
+		else if (type == (u_short)ePacketType::TimeOut)
+		{
+			u_short choice = input - '0';
+			tPacketData data = WritePacket<u_short>(ePacketType::TimeOut, choice);
+			send(hSocket, data.body, data.size, 0);
+			elapsed = 0.0f;
+			gamePlaying = false;
+		}
+		temp += sizeof(u_short);
+	}
+
+	return -1;
+}
+
+void Input()
+{
+	if (_kbhit())
+	{
+		char ch = _getch();
+
+		if (ch == 27) // esc키
+		{
+			u_short temp = 0;
+			tPacketData data = WritePacket<u_short>(ePacketType::PlayerDisconnect, temp);
+			send(hSocket, data.body, data.size, 0);
+		}
+		else
+		{
+			if ('1' <= ch && ch <= '3')
+				input = ch;
+		}
+
+		PrintBoard();
+	}
+}
+
+void Update()
+{
+	while (!recvList.empty())
+	{
+		delete[] recvList.front();
+		recvList.pop();
+	}
+
 }
--- TestServer/TestServer/TestServer.cpp
+++ TestServer/TestServer/TestServer.cpp
@@ -3,30 +3,287 @@
 #include <ctime>
 #include <vector>
 #include <conio.h>
-#include <process.h>
+#include <process.h>+
 #include <stdio.h>
 #include <list>
+#include <chrono>
 
 using namespace std;
 
 #pragma comment( lib, "ws2_32.lib")
 
-list<const char*> conversationList;
+enum class ePacketType
+{
+	PlayerEnter,
+	PlayerExit,
+	PlayerConnect,
+	PlayerDisconnect,
+	TimeOut,
+	GameStart,
+	GameEnd
+};
+
+typedef struct _packetData
+{
+	int size;
+	char* body;
+} tPacketData;
+
 char input[100] = "";
 int i = 0;
 char random;
 SOCKET   hSocket;
 
 vector<SOCKET> listSocket;
-struct tData { int loc; SOCKET soc; };
+vector<const char*> listMsg;
 
-void SendAll(char* str);
+int playerScore[2];
+u_short playerChoice[2];
+const char* printChoice[4] = { "선택안함", "가위", "바위", "보" };
+bool gameStart = false;
+bool gamePlaying = false;
+bool gameEnd = false;
+bool isUsed = false;
+int playerChoiceCount = 0;
+HANDLE g_updateThreadHandle;
+
+LARGE_INTEGER timer, start, end1;
+float deltaTime, elapsed;
+
+template<typename T>
+tPacketData WritePacket(ePacketType what, T& body);
+int ReadPacket(char* body, int size, SOCKET s);
+void SendAll(tPacketData str, SOCKET exceptclientSocket = -1);
+unsigned int __stdcall Comm(void* _pArgs);
+int Init();
+void PrintBoard();
+unsigned int __stdcall Update(void* _pArgs);
+
+#define TIMEOUT			10.0f
+
+int main(void)
+{
+	if (Init() == -1) return -1;
+
+	SOCKADDR_IN		clientAddr;
+	SOCKET clientSocket;
+	int			sizeClientAddr = sizeof(clientAddr);
+
+	srand(time(nullptr));
+
+	std::puts("서버 실행중");
+
+	unsigned int threadID;
+	HANDLE hThread;
+
+	while (1)
+	{
+		PrintBoard();
+
+		clientSocket = accept(hSocket, (SOCKADDR*)&clientAddr, &sizeClientAddr);
+		if (clientSocket == SOCKET_ERROR)
+		{
+			printf("Failed accept() \n");
+			return -1;
+		}
+
+		int listSocketSize = listSocket.size();
+
+		if (listSocketSize < 1)
+		{
+			g_updateThreadHandle = (HANDLE)_beginthreadex(NULL, 0, &Update, nullptr, 0, &threadID);
+			if (g_updateThreadHandle == INVALID_HANDLE_VALUE) return 0;
+		}
+
+		listSocket.push_back(clientSocket);
+		listSocketSize++;
+
+
+
+		u_short client = (u_short)clientSocket;
+		tPacketData data = WritePacket<u_short>(ePacketType::PlayerEnter, client);
+		SendAll(data, clientSocket); 
+
+		u_short size = sizeof(u_short) + (listSocketSize * sizeof(u_short));
+		char* packet = new char[sizeof(u_short) + size]();
+		char* temp = packet;
+		memcpy(temp, &size, sizeof(u_short));
+		temp += sizeof(u_short);
+		u_short what = (u_short)ePacketType::PlayerConnect;
+		memcpy(temp, &what, sizeof(u_short));
+		temp += sizeof(u_short);
+		for (int i = 0; i < listSocketSize; i++)
+		{
+			client = (u_short)listSocket[i];
+			*(u_short*)temp = client;
+			temp += sizeof(u_short);
+		}
+
+		send(clientSocket, packet, size + sizeof(u_short), 0);
+		//for(int i=0;i<size+sizeof(u_short);i++)
+		//	printf("%02x ", packet[i]);
+		//puts("");
+
+		if (!gameStart && listSocketSize >= 2)
+		{
+			gameStart = true;
+		}
+
+		delete[] data.body;
+
+		hThread = (HANDLE)_beginthreadex(NULL, 0, &Comm, (void*)&clientSocket, 0, &threadID);
+		if (hThread == INVALID_HANDLE_VALUE) return 0;
+	}
+
+	std::puts("서버 종료");
+
+	closesocket(hSocket);
+	WSACleanup();
+
+	return 0;
+}
+
+
+template<typename T>
+tPacketData WritePacket(ePacketType what, T& body)
+{
+	u_short size = sizeof(u_short) + sizeof(body);
+	char* packet = new char[size + sizeof(u_short)];
+	char* temp = packet;
+	memcpy(temp, &size, sizeof(u_short));
+	temp += sizeof(u_short);
+	memcpy(temp, &what, sizeof(u_short));
+	temp += sizeof(u_short);
+	*(u_short*)temp = body;
+	tPacketData data = { size + sizeof(u_short), packet };
+	return data;
+}
+
+int ReadPacket(char* body, int size, SOCKET s)
+{
+	char* temp = body;
+	u_short header = *(u_short*)temp;
+	if (header != size - sizeof(u_short)) return -1;
+
+	temp += sizeof(u_short);
+	u_short type = *(u_short*)temp;
+	temp += sizeof(u_short);
+
+	if (type == (u_short)ePacketType::PlayerEnter) // 누군가 처음 들어왔을떄
+	{
+
+	}
+	else if (type == (u_short)ePacketType::PlayerExit)
+	{
+
+	}
+	else if (type == (u_short)ePacketType::PlayerConnect)
+	{
+
+	}
+	else if (type == (u_short)ePacketType::PlayerDisconnect)
+	{
+		u_short t = 0;
+		tPacketData data = WritePacket<u_short>(ePacketType::PlayerDisconnect, t);
+		send(s, data.body, data.size, 0);
+		int i = 0;
+
+		vector<SOCKET>::iterator iter = listSocket.begin();
+		vector<SOCKET>::iterator iterEnd = listSocket.end();
+		vector<SOCKET>::iterator target;
+
+		for (; iter != iterEnd; ++iter, i++)
+		{
+			if (*iter == s) // [0] or [1]이 나갈때만 0초로 초기화
+			{
+				target = iter;
+				closesocket(s);
+				CloseHandle(GetCurrentThread());
+				if (i == 0 || i == 1)
+					elapsed = 0.0f;
+
+				if (i == 0) // [0]이 나간 경우 [1]에 있던 점수가 [0]으로 이동
+				{
+					playerScore[0] = playerScore[1];
+					playerScore[1] = 0;
+				}
+			}
+			else
+			{
+				u_short t = s;
+				tPacketData data = WritePacket<u_short>(ePacketType::PlayerExit, t);
+				send(s, data.body, data.size, 0);
+			}
+		}
+
+		listSocket.erase(target);
+
+		int listSocketSize = listSocket.size();
+		if (listSocketSize < 2)
+		{
+			gamePlaying = false;
+			elapsed = 0.0f;
+			u_short t = 0;
+			tPacketData data = WritePacket<u_short>(ePacketType::GameEnd, t);
+			for (int i = 0; i < listSocketSize; i++)
+				send(listSocket[i], data.body, data.size, 0);
+		}
+		PrintBoard();
+	}
+	else if (type == (u_short)ePacketType::TimeOut)
+	{
+		int i = 0;
+		vector<SOCKET>::iterator iter = listSocket.begin();
+		vector<SOCKET>::iterator iterEnd = listSocket.end();
+
+		for (; iter != iterEnd; iter++, i++)
+		{
+			if (listSocket[i] == s)
+			{
+				playerChoice[i] = *(u_short*)temp;
+				playerChoiceCount++;
+				break;
+			}
+		}
+	}
+	return -1;
+}
+
+void PrintBoard()
+{
+	if (!isUsed)
+	{
+		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,0 });
+		isUsed = true;
+		//vector<SOCKET>::iterator iterEnd = listSocket.end();
+		int size = listSocket.size();
+		system("cls");
+		std::puts("================== 가위바위보 게임 ==================");
+		//if (iter == iterEnd) puts("");
+		if (size == 0) std::puts("");
+		//for(int i=1; iter != iterEnd && i <= 2; iter++, i++)
+		int i = 0;
+		for (; i <= 1; i++)
+			if (i <= (size - 1))
+				printf("플레이어%d : %d --------> %s \t|\t 현재 승리한 횟수 : %d회\n", i + 1, (int)listSocket[i], printChoice[playerChoice[i]], playerScore[i]);
+		std::puts("================== 대기열 ==================");
+		if (size > 2)
+		{
+			vector<SOCKET>::iterator iter = listSocket.begin() + i;
+			vector<SOCKET>::iterator endIter = listSocket.end();
+			for (; iter != endIter; iter++) // for문 출력 도중 소켓 하나가 연결이 끊어지면 listSocket의 크기가 1 줄어드는데, 매번 listSocket.end() 호출
+				printf("%d ", (int)*iter);
+		}
+		std::puts("\n============================================");
+		//if (gamePlaying)
+		//	printf("타이머 : %f\n", TIMEOUT - elapsed);
+		isUsed = false;
+	}
+}
 
 unsigned int __stdcall Comm(void* _pArgs)
 {
-	tData* data = (tData*)_pArgs;
-	SOCKET s = data->soc;
-	int loc = data->loc;
+	SOCKET s = *((SOCKET*)_pArgs);
 
 	while (1)
 	{
@@ -36,38 +293,23 @@ unsigned int __stdcall Comm(void* _pArgs)
 
 		if (tempSize != SOCKET_ERROR)
 		{
-			char* inputTemp = new char[strlen(temp) + 1];
-			strcpy_s(inputTemp, strlen(temp) + 1, temp);
-			conversationList.push_back(inputTemp);
-			if (strcmp("▒", inputTemp) == 0)
-			{
-				CloseHandle(GetCurrentThread());
-				send(s, "▒", strlen("▒") + 1, 0);
-				closesocket(s);
-				listSocket.erase(listSocket.begin() + loc);
-				printf("================ %d소켓 제거\n", (int)s);
-				break;
-			}
-			printf("%d소켓으로부터 메시지 : %s\n", (int)s, inputTemp);
-			SendAll(inputTemp);
-			if (conversationList.size() > 10)
-			{
-				delete[] conversationList.front();
-				conversationList.pop_front();
-			}
+			char* inputTemp = new char[tempSize];
+			memcpy(inputTemp, &temp, tempSize);
+
+			ReadPacket(inputTemp, tempSize, s);
 		}
 	}
-	return -1;
 }
 
 
-void SendAll(char* str)
+void SendAll(tPacketData data, SOCKET exceptclientSocket)
 {
-	for(int i=0;i<listSocket.size(); i++)
-		send(listSocket[i], str, strlen(str) + 1, 0);
+	for (int i = 0; i < listSocket.size(); i++)
+		if (exceptclientSocket != listSocket[i])
+			send(listSocket[i], data.body, data.size, 0);
 }
 
-int main(void)
+int Init()
 {
 	WSADATA  wsaData;
 	SOCKADDR_IN  servAddr;
@@ -100,40 +342,116 @@ int main(void)
 		return -1;
 	}
 
-	SOCKADDR_IN		clientAddr;
-	SOCKET clientSocket;
-	int			sizeClientAddr = sizeof(clientAddr);
+	QueryPerformanceFrequency(&timer);
+}
 
-	puts("서버 실행중");
+// 1 : 가위
+// 2 : 바위
+// 3 : 보
+void Game()
+{
+	if (playerChoice[0] <= 0 || playerChoice[0] >= 4)
+		playerChoice[0] = (rand() % 3) + 1;
+	if (playerChoice[1] <= 0 || playerChoice[1] >= 4)
+		playerChoice[1] = (rand() * rand() % 3) + 1;
 
+	if (playerChoice[0] == 1)
+	{
+		if (playerChoice[1] == 2) playerScore[1]++;
+		else if (playerChoice[1] == 3) playerScore[0]++;
+	}
+	else if (playerChoice[0] == 2)
+	{
+		if (playerChoice[1] == 1) playerScore[0]++;
+		else if (playerChoice[1] == 3) playerScore[1]++;
+	}
+	else if (playerChoice[0] == 3)
+	{
+		if (playerChoice[1] == 1) playerScore[1]++;
+		else if (playerChoice[1] == 2) playerScore[0]++;
+	}
+}
 
+unsigned int __stdcall Update(void* _pArgs)
+{
 	while (1)
 	{
-		clientSocket = accept(hSocket, (SOCKADDR*)&clientAddr, &sizeClientAddr);
-		if (clientSocket == SOCKET_ERROR)
+		int listSocketSize = listSocket.size();
+
+		if (listSocketSize <= 0) break;
+
+		if (gameEnd)
 		{
-			printf("Failed accept() \n");
-			return -1;
+			QueryPerformanceCounter(&end1);
+			deltaTime = (end1.QuadPart - start.QuadPart) / (float)timer.QuadPart;
+			elapsed += deltaTime; // 서버에서 stop할 때까지 계속 측정
+
+			if (elapsed >= TIMEOUT - 5) // 5초간 쉬는시간 후 
+			{
+				if (!gameStart && listSocketSize >= 2)
+					gameStart = true;
+				gameEnd = false;
+				elapsed = 0.0f;
+			}
 		}
 
-		listSocket.push_back(clientSocket);
-		int loc = listSocket.size() - 1;
-		printf("================ %d 소켓 접속\n", (int)clientSocket);
+		if (gameStart && listSocketSize >= 2)
+		{
+			u_short soc;
+			ePacketType type;
+			tPacketData data;
+			type = ePacketType::GameStart;
 
-		tData data;
-		data.loc = loc;
-		data.soc = clientSocket;
+			for (int i = 0; i < listSocketSize; i++)
+			{
+				soc = (u_short)listSocket[i];
+				data = WritePacket<u_short>(type, soc);
+				send(listSocket[i], data.body, data.size, 0);
+			}
 
-		unsigned int threadID;
-		HANDLE hThread;
-		hThread = (HANDLE)_beginthreadex(NULL, 0, &Comm, (void*)&data, 0, &threadID);
-		if (hThread == INVALID_HANDLE_VALUE) return 0;
+			QueryPerformanceCounter(&start);
+			gameStart = false;
+			gamePlaying = true;
+		}
+
+		if (gamePlaying)
+		{
+			Sleep(1);
+			QueryPerformanceCounter(&end1);
+			deltaTime = (end1.QuadPart - start.QuadPart) / (float)timer.QuadPart;
+			elapsed += deltaTime; // 서버에서 stop할 때까지 계속 측정
+
+
+			if (elapsed >= TIMEOUT)
+			{
+				u_short soc;
+				tPacketData data;
+				ePacketType type;
+				for (int i = 0; i < listSocketSize; i++) // SendAll 사용하기
+				{
+					soc = (u_short)listSocket[i];
+					data = WritePacket<u_short>(ePacketType::TimeOut, soc);
+					send(listSocket[i], data.body, data.size, 0);
+				}
+				elapsed = 0.0f;
+				gameEnd = true;
+				gamePlaying = false;
+
+				while (playerChoiceCount < 2) { ; }
+				playerChoiceCount = 0;
+				Game();
+			}
+		}
+		if (!isUsed)
+		{
+			SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), { 0,14 });
+			printf("                               ");
+			printf("%s타이머 : %.2f               \n", !gamePlaying ? "쉬는시간 " : "", (gamePlaying ? TIMEOUT - elapsed : 5.0f - elapsed));
+		}
+		start = end1;
+		//PrintBoard();
 	}
 
-	puts("서버 종료");
-
-	closesocket(hSocket);
-	WSACleanup();
-
-	return 0;
+	CloseHandle(GetCurrentThread());
+	return -1;
 }
